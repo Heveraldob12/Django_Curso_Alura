c1f542d58320f7b2ba1496078735186f9c741823
Heveraldo <heveraldob12@gmail.com>
2023-02-14 12:56:52 -0300

    Aula 2 | Curso: Django - autenticação de formulários e alerta

---
+++ usuarios/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from usuarios.forms import LoginForms, CadastroFroms
+
+def login(request):
+    form = LoginForms()
+    return render(request, 'usuarios/login.html', { 'form' :form})
+
+def cadastro(request):
+    form = CadastroFroms()
+    return render(request, 'usuarios/cadastro.html', { 'form' :form})
