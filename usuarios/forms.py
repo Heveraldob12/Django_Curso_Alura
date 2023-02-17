22418e17d930a853b79912850402994c635269da
Heveraldo <heveraldob12@gmail.com>
2023-02-17 10:29:16 -0300

    Aula Final | Curso: Django - autenticação de formulários e alerta

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -63,4 +63,21 @@ class CadastroFroms(forms.Form):
                         'class': 'form-control',
                         'placeholder': 'Digite sua senha novamente',
                     }),)
-    
+    def clean_nome_cadastro(self):
+        nome= self.cleaned_data.get("nome_cadastro")
+        if nome:
+            nome = nome.strip()
+            if " " in nome:
+                raise forms.ValidationError("Não é possivel inserir espaços dentro desse campo")
+            else:
+                return nome
+    def clean_senha_2(self):
+        senha_1= self.cleaned_data.get("senha_1")
+        senha_2= self.cleaned_data.get("senha_2")
+        
+        if senha_1 and senha_2:
+            if senha_1 !=senha_2:
+                raise forms.ValidationError("Senha não são iguais")
+            else:
+                return senha_2
+        
